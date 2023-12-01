b0de665317ad866e5bfe36716dec1b364df19598
Kevin <kevin@9troisquarts.com>
2023-12-01 18:23:35 +0100

    fix: Week

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,19 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import weekday from 'dayjs/plugin/weekday';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
 import weekYear from 'dayjs/plugin/weekYear';
+import isoWeek from 'dayjs/plugin/isoWeek';
 
 dayjs.extend(customParseFormat);
 dayjs.extend(advancedFormat);
 dayjs.extend(weekday);
 dayjs.extend(localeData);
 dayjs.extend(weekOfYear);
+dayjs.extend(isoWeek);
 dayjs.extend(weekYear);
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
 
-dayjs().format('L LT');
+dayjs().format('L LT LLL');
 
 import AntForm from './ant-form';
 export type { AntSchema as AntSchema } from './ant-form';
